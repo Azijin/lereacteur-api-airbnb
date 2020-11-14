a6456599b3b3bd075256d2ed17d63b5180f6890e
Arthur Chen <arthur@MacBook-Pro-de-Arthur.local>
2020-11-14 18:06:04 +0100

    Feat : fixes the middleware to add the token user in the request

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -3,7 +3,9 @@ const isAuthenticated = async (req, res, next) => {
   try {
     if (req.headers.authorization) {
       const token = req.headers.authorization.replace("Bearer ", "");
-      const user = await User.findOne({ token: token }).select("account _id");
+      const user = await User.findOne({ token: token }).select(
+        "account _id token"
+      );
       if (user) {
         req.user = user;
         return next();
