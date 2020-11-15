4c47a80745905f20c6cadcba81b6e35f9f4bb956
Arthur Chen <arthur@MacBook-Pro-de-Arthur.local>
2020-11-15 15:15:56 +0100

    Feat: add isAuthorized middleware in update and delete routes

--- routes/room.js
+++ routes/room.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const User = require("../models/User");
 const Room = require("../models/Room");
 const isAuthenticated = require("../middlewares/isAuthenticated");
+const isAuthorized = require("../middlewares/isAuthorized");
 
 router.post("/room/publish", isAuthenticated, async (req, res) => {
   try {
@@ -19,6 +20,9 @@ router.post("/room/publish", isAuthenticated, async (req, res) => {
         photos: [],
       });
       await newRoom.save();
+      const userRooms = user.rooms;
+      userRooms.push(newRoom._id);
+      await User.findByIdAndUpdate(user._id, { rooms: userRooms });
       res.status(200).json(newRoom);
     } else {
       res.status(400).json({ error: "Missing parameters" });
@@ -29,7 +33,10 @@ router.post("/room/publish", isAuthenticated, async (req, res) => {
 });
 
 router.get("/rooms", async (req, res) => {
-  const rooms = await Room.find().select("-description");
+  const rooms = await Room.find().select("-description").polulate({
+    path: "user",
+    select: "account",
+  });
   if (rooms) {
     res.status(200).json(rooms);
   } else {
@@ -38,89 +45,87 @@ router.get("/rooms", async (req, res) => {
 });
 
 router.get("/rooms/:id", async (req, res) => {
-  try {
-    const offer = await Room.findById(req.params.id).populate({
-      path: "user",
-      select: "account _id",
-    });
-    if (offer) {
-      res.status(200).json(offer);
-    } else {
-      res.status(400).json({ error: "Offer not found" });
-    }
-  } catch (error) {
-    res.status.json({ error: error.message });
-  }
-});
-
-router.put("/room/update/:id", isAuthenticated, async (req, res) => {
-  try {
-    const user = req.user;
-    const offer = await Room.findById(req.params.id).populate({
-      path: "user",
-      select: "-hash -salt",
-    });
-    if (offer) {
-      if (user.token === offer.user.token) {
-        const { title, description, price, location } = req.fields;
-        if (title || description || price || location || location) {
-          if (title) {
-            offer.title = title;
-          }
-          if (description) {
-            offer.description = description;
-          }
-          if (price) {
-            offer.price = price;
-          }
-          if (location) {
-            if (location.lat) {
-              offer.location.shift();
-              offer.location.unshift(location.lat);
-            }
-            if (location.lng) {
-              offer.location.pop();
-              offer.location.push(location.lng);
-            }
-          }
-          await offer.save();
-          const offerToDisplay = await Room.findById(offer._id).populate({
-            path: "user",
-            select: "account",
-          });
-          res.status(200).json(offerToDisplay);
-        } else {
-          res.status(400).json({ error: "Missing parameters for update" });
-        }
+  if (req.params.id) {
+    try {
+      const room = await Room.findById(req.params.id).populate({
+        path: "user",
+        select: "account",
+      });
+      if (room) {
+        res.status(200).json(room);
       } else {
-        res.status("400").json({ error: "Unauthorized" });
+        res.status(400).json({ error: "Room not found" });
       }
-    } else {
-      res.status(400).json({ error: "Offer not found" });
+    } catch (error) {
+      res.status.json({ error: error.message });
     }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+  } else {
+    res.status(400).json({ error: "Missing room id" });
   }
 });
 
-router.delete("/room/delete/:id", isAuthenticated, async (req, res) => {
-  try {
-    const offer = await Room.findById(req.params.id).populate({
-      path: "user",
-      select: "token",
-    });
-    const user = req.user;
-    if (offer) {
-      if (offer.user.token === user.token) {
-        await offer.delete();
-        res.status(200).json({ message: "Room deleted" });
+router.put(
+  "/room/update/:id",
+  isAuthenticated,
+  isAuthorized,
+  async (req, res) => {
+    try {
+      const { title, description, price, location } = req.fields;
+      const user = req.user;
+      const room = req.room;
+      if (title || description || price || location) {
+        if (title) {
+          room.title = title;
+        }
+        if (description) {
+          room.description = description;
+        }
+        if (price) {
+          room.price = price;
+        }
+        if (location) {
+          if (location.lat) {
+            room.location.shift();
+            room.location.unshift(location.lat);
+          }
+          if (location.lng) {
+            room.location.pop();
+            room.location.push(location.lng);
+          }
+        }
+        await room.save();
+        /*const roomToDisplay = await Room.findById(room._id).populate({
+          path: "user",
+          select: "account",
+        });*/
+        res.status(200).json(room);
+      } else {
+        res.status(400).json({ error: "Missing parameters for update" });
       }
-    } else {
-      res.status(400).json({ error: "Offer not found" });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
   }
-});
+);
+
+router.delete(
+  "/room/delete/:id",
+  isAuthenticated,
+  isAuthorized,
+  async (req, res) => {
+    try {
+      const room = req.room;
+      await room.delete();
+      const user = req.user;
+      const userRooms = user.rooms;
+      const indexRoom = userRooms.indexOf(room._id);
+      userRooms.splice(indexRoom, 1);
+      await User.findByIdAndUpdate(user._id, { rooms: userRooms });
+      res.status(200).json({ message: "Room deleted" });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+);
 
 module.exports = router;
