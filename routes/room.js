5016f7630176ceac6435cf5498f5832828f29ccd
Arthur Chen <arthur@MacBook-Pro-de-Arthur.local>
2020-11-16 18:13:19 +0100

    feat: add routes to delete picture of a room

--- routes/room.js
+++ routes/room.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const cloudinary = require("cloudinary").v2;
 
 const User = require("../models/User");
 const Room = require("../models/Room");
@@ -93,13 +94,90 @@ router.put(
           }
         }
         await room.save();
-        /*const roomToDisplay = await Room.findById(room._id).populate({
-          path: "user",
-          select: "account",
-        });*/
-        res.status(200).json(room);
+        const roomToDisplay = await Room.findById(room._id);
+        res.status(200).json(roomToDisplay);
       } else {
         res.status(400).json({ error: "Missing parameters for update" });
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+);
+
+router.put(
+  "/room/upload_picture/:id",
+  isAuthenticated,
+  isAuthorized,
+  async (req, res) => {
+    try {
+      if (req.files.picture) {
+        const room = req.room;
+        const photosRoom = room.photos;
+        if (photos.length < 5) {
+          const photo = req.files.picture.path;
+          const user = req.user;
+          await cloudinary.uploader.upload(
+            photo,
+            {
+              folder: `airbnb/users/${user._id}/rooms/${room._id}`,
+            },
+            async function (error, result) {
+              const newPhoto = {
+                url: result.secure_url,
+                picture_id: result.public_id,
+              };
+              photosRoom.push(newPhoto);
+            }
+          );
+          await Room.findByIdAndUpdate(room._id, {
+            photos: photosRoom,
+          });
+          const roomUpdated = await Room.findById(room._id);
+          res.status(200).json(roomUpdated);
+        } else {
+          res.status(400).json({ error: "Can't add more than 5 pictures" });
+        }
+      } else {
+        res.status(400).json({ error: "Missing picture" });
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+);
+
+router.put(
+  "/room/delete_picture/:id",
+  isAuthenticated,
+  isAuthorized,
+  async (req, res) => {
+    try {
+      if (req.fields.picture_id) {
+        const room = req.room;
+        const photosRoom = room.photos;
+        const picture_id = req.fields.picture_id;
+        let isPhoto = false;
+        for (let i = 0; i < photosRoom.length; i++) {
+          if (photosRoom[i].picture_id === picture_id) {
+            isPhoto = true;
+          }
+        }
+        if (!isPhoto) {
+          res.status(400).json({ error: "Picture not found" });
+        } else {
+          for (let j = 0; j < photosRoom.length; j++) {
+            if (photosRoom[j].picture_id === picture_id) {
+              const index = photosRoom.indexOf(photosRoom[j]);
+              photosRoom.slice(index, 1);
+              await cloudinary.uploader.destroy(picture_id);
+              await Room.findByIdAndUpdate(room._id, { photos: photosRoom });
+            }
+          }
+          res.status(200).json({ message: "Picture deleted" });
+        }
+      } else {
+        res.status(400).json({ error: "Missing picture id" });
       }
     } catch (error) {
       res.status(400).json({ error: error.message });
